15c6c36c1c2b148da67b5e5a25d2fa8fbc59aa24
Bhavik Patel <Bhavikpatel576@gmail.com>
2017-12-13 13:02:26 -0600

    add shout list to dashboard

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,20 @@
+class ShoutsController < ApplicationController
+  def create
+    @shout = current_user.shouts.create(shout_params)
+    redirect_to root_path, redirect_option_for(@shout)
+  end
+
+  private
+
+  def shout_params
+    params.require(:shout).permit(:body)
+  end
+
+  def redirect_option_for(value)
+    if value.persisted?
+      { notice: "You've successfully Shouted" }
+    else
+      { alert: "Something went wrong with your Shout" }
+    end
+  end
+end
